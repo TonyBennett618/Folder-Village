8ff3e19a02fe12d290d696cc62cbc43abe72bd7a
TonyBennett618 <bennettaa2@vcu.edu>
2022-04-13 18:22:50 -0400

    Changes
    Folder addition, dialogue test

--- Village/functions.js
+++ Village/functions.js
@@ -1,17 +1,50 @@
 function cyDialogue(){
-    var name = document.getElementById("Cy")
+    var modal = document.getElementById("cyModal");
+    
+    // Get the button that opens the modal
+    var btn = document.getElementById("Cy");
 
-    var alertMessage = "Oh, I've not seen you around here! Welcome to Crewnage Village. Do you need any assistance? (Ok for yes, Cancel for no)"
-    var r = confirm(alertMessage);
 
-    if(r==true)
-    {
-        alertMessage=("Ok! Well, we have an Inn, if you need a room to sleep. We have a tavern if you want a load off, and we have a market! Where you can spend your money! If you need me, I will be right here!")
+
+    // Get the <span> element that closes the modal
+    var span = document.getElementsByClassName("close")[0];
+
+    // When the user clicks the button, open the modal 
+    Cy.onclick = function() {
+    modal.style.display = "block"
     }
-    else
-    {
-        alertMessage=("Oh, I see, well I hope you find what you are looking for.")
+
+    // When the user clicks on <span> (x), close the modal
+    span.onclick = function() {
+    modal.style.display = "none";
     }
+
+    // When the user clicks anywhere outside of the modal, close it
+    window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+    } 
+    // this is for dialogue etc
+
+};
+
+
+function cyDialogueYes(){
+    var prevModal = document.getElementById("cyModal");
+    var nextModal = document.getElementById("cyModalYes");
+    var hidden = document.getElementById("hidden");
+    var yes = document.getElementsByClassName("yes");
+
+    // Get the <span> element that closes the modal
+    var span = document.getElementsByClassName("close")[0];
+
+    yes.onclick = function() {
+        nextModal.style.display = "block";
+    
+    }
+
+
 };
 
 function goToSleep(){
