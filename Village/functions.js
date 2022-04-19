bd6f20de7d94692ade3f19d15c24091ea78aa610
TonyBennett618 <bennettaa2@vcu.edu>
2022-04-19 18:40:32 -0400

    Adding tavern bartender function

--- Village/functions.js
+++ Village/functions.js
@@ -1,6 +1,10 @@
+//Bathroom.html functions
 
 
-//past this point is dialogue
+
+
+
+//past this point is dialogue functions
 var current = document.getElementsByClassName("current");
 var okBtn = document.getElementsByClassName("ok");
 var span = document.getElementsByClassName("close");
@@ -171,6 +175,97 @@ function innDialogueYes(){
 function innDialogueNo(){
   var prevModal = document.getElementById("innModal");
   var nextModal = document.getElementById("innModalNo");
+  var ok = document.getElementsByClassName("ok");
+
+
+  // Get the <span> element that closes the modal
+  // var span = document.getElementsByClassName("close")[1];
+
+  span.onclick = function() {
+  nextModal.style.display = "none";
+  }
+
+  window.onclick = function(event) {
+  if (event.target == nextModal) {
+      nextModal.style.display = "none";
+  }
+  }
+
+  //alert("Sup")
+  prevModal.classList.remove("current")
+  nextModal.classList.add("current")
+  prevModal.style.display = "none"
+  nextModal.style.display = "block"
+
+  ok.onclick = function(){
+  nextModal.style.display = "none";
+  }
+};
+
+//bartender dialogue
+
+function barDialogue(){
+    var modal = document.getElementById("barModal");
+
+    // Get the button that opens the modal
+    var btn = document.getElementById("Bartender");
+
+    // Get the <span> element that closes the modal
+    // var span = document.getElementsByClassName("close")[0];
+
+    // When the user clicks the button, open the modal
+    Bartender.onclick = function() {
+    modal.classList.add("current")
+    modal.style.display = "block"
+    }
+
+    // When the user clicks on <span> (x), close the modal
+    span.onclick = function() {
+    modal.style.display = "none";
+    }
+
+    // When the user clicks anywhere outside of the modal, close it
+    window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+    }
+};
+
+
+function barDialogueYes(){
+    var prevModal = document.getElementById("barModal");
+    var nextModal = document.getElementById("barModalYes");
+    var ok = document.getElementsByClassName("ok");
+
+
+    // Get the <span> element that closes the modal
+    // var span = document.getElementsByClassName("close")[1];
+
+    span.onclick = function() {
+    nextModal.style.display = "none";
+    }
+
+    window.onclick = function(event) {
+    if (event.target == nextModal) {
+        nextModal.style.display = "none";
+    }
+    }
+
+    //alert("Sup")
+    prevModal.classList.remove("current")
+    nextModal.classList.add("current")
+    prevModal.style.display = "none"
+    nextModal.style.display = "block"
+
+    ok.onclick = function(){
+    nextModal.style.display = "none";
+    }
+};
+
+function barDialogueNo(){
+  var prevModal = document.getElementById("barModal");
+  var nextModal = document.getElementById("barModalNo");
   var ok = document.getElementsByClassName("ok");
 
 
