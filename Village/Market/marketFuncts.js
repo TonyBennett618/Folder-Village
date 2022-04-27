b8d357c862163e75dafa01922486350979900f20
TonyBennett618 <bennettaa2@vcu.edu>
2022-04-26 21:40:32 -0400

    Adding functions, vivify

--- Village/Market/marketFuncts.js
+++ Village/Market/marketFuncts.js
@@ -1,6 +1,7 @@
+var anima = document.getElementById("coin")
+
 function purchaseFruitAndCheese() {
     var stand = document.getElementById("fruitAndCheese")
-    var anima = document.getElementById("coin")
     var anim = document.getElementsByClassName("animation-container")
     if (confirm("Purchase item?") == true) {
   
@@ -10,15 +11,64 @@ function purchaseFruitAndCheese() {
       anime({
         targets: '.animation-container',
         delay: 5000,
-        translateY: -500
+        translateY: -600,
+        easing: 'easeInOutSine',
       });
       const timeout = setTimeout(anima, 7000)
       function anima(){
             coin.classList.remove("visible");
-            alert("begone thot")
       }
+
       alert("You have purchased half a dozen apples, and some goat cheese.")
     } else {
       alert("You decide not to buy anything.");
     }
-  }
+  };
+function purchaseFruitAndCheese() {
+    var stand = document.getElementById("fruitAndCheese")
+    var anim = document.getElementsByClassName("animation-container")
+    if (confirm("Purchase item?") == true) {
+  
+      //play sleep anim (create animation)
+      coin.classList.add("visible");
+    
+      anime({
+        targets: '.animation-container',
+        delay: 5000,
+        translateY: -600,
+        easing: 'easeInOutSine',
+      });
+      const timeout = setTimeout(anima, 7000)
+      function anima(){
+            coin.classList.remove("visible");
+      }
+
+      alert("You have purchased half a dozen apples, and some goat cheese.")
+    } else {
+      alert("You decide not to buy anything.");
+    }
+  };
+function purchaseTrinket() {
+    var stand = document.getElementById("trinkets")
+    var anim = document.getElementsByClassName("animation-container")
+    if (confirm("Purchase item?") == true) {
+  
+      //play sleep anim (create animation)
+      coin.classList.add("visible");
+    
+      anime({
+        targets: '.animation-container',
+        delay: 5000,
+        translateY: -600,
+        easing: 'easeInOutSine',
+      });
+      const timeout = setTimeout(anima, 7000)
+      function anima(){
+            coin.classList.remove("visible");
+      }
+
+      alert("You have purchased an silver amulet with a small switch on the back of it. Maybe it opens something.")
+    } else {
+      alert("You decide not to buy anything.");
+    }
+  };
